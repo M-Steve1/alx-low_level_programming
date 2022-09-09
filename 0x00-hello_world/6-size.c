ceb854beb28f55d82e38028550437240f7d8ebf0
M-Steve1 <modebestephen2@gmail.com>
2022-09-08 20:34:28 -0700

    Update 6-size.c script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,6 +18,7 @@ int main(void)
 	printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu.\n", (unsigned long)sizeof(li));
 	printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(lli));
-	printf("Size of a float: %lu.\n," (unsigned long)sizeof(f);
+	printf("Size of a float: %lu.\n", (unsigned long)sizeof(f));
+
 	return (0);
 }
