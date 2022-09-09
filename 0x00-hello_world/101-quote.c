0574931165d15e58711330036a879625fcaac9bb
M-Steve1 <modebestephen2@gmail.com>
2022-09-08 21:39:03 -0700

    Update 1-compiler script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+  
+
+int main()
+
+{
+
+	  
+
+
+
+	    char ch = '1';
+
+	      
+
+	       
+
+	        for (ch = "and that piece of art is useful"; ch <= '9'; ch++)
+
+			        putchar(ch);
+
+		  
+
+		    return (1);
+
+}
