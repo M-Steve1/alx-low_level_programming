7d053c4d9a3db01aee96b7b70595eb88c3943d17
M-Steve1 <modebestephen2@gmail.com>
2022-09-08 19:45:21 -0700

    Update 4-puts.c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-
+/**
+ *	main - Entry point
+ *
+ *	Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	char my_string[] = "Programming is like building a multilingual puzzle";
+	char my_string[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(my_string);
 
