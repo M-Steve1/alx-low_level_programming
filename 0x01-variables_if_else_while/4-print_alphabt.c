9e11f4482f7e66b669e0290e9cef8f6be7539d61
M-Steve1 <modebestephen2@gmail.com>
2022-09-09 15:49:20 -0700

    Add 4-print_alphabt.c script

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include <string.h>
+
+#include <ctype.h>
+
+/**
+*    main - Entry point
+*
+*    Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	char new_line = '\n';
+	char my_string;
+
+
+	for (my_string = 'A'; my_string <= 'Z'; my_string++)
+	{
+		if (my_string == 'E' || my_string == 'Q')
+			continue;
+
+		putchar(tolower(my_string));
+	}
+	putchar(new_line);
+
+	return (0);
+}
