45d4d85a5716fde8783f42c8f643e0cbe3fa0e58
M-Steve1 <modebestephen2@gmail.com>
2022-09-09 21:15:19 -0700

    Add 102-print_comb5.c script

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ *	main - Entry point
+ *
+ *	Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48; i <= 59; i++)
+	{
+		for (j = 48; j <= 56; j++)
+		{
+			for (k = 48; k <= 59; k++)
+			{
+				for (l = 47; l <= 59; l++)
+				{
+					if (l + k > j + i)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+
+						if (i == 59 && j == 57 && k == 59 && l == 59)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
