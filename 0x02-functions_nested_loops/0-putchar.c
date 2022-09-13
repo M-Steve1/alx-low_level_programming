a290ada9f51094913f6a5d9b9dbb4b2447bc5e94
M-Steve1 <modebestephen2@gmail.com>
2022-09-13 16:23:07 -0700

    0. _putchar task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,16 +8,18 @@
  * Returns 0 (Success)
  */
 
-int main()
+int main(void)
 {
-  _putchar("_");
-  _putchar("p");
-  _putchar("u");
-  _putchar("t");
-  _putchar("c");
-  _putchar("h");
-  _putchar("a");
-  _putchar("r");
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+  
 
   return 0;
 }
