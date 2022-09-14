cf7fd9a153a4484160f0628c4ebc407ab79489d6
M-Steve1 <modebestephen2@gmail.com>
2022-09-14 03:06:02 -0700

    Add c files

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024 (excluded), followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int sum_three, sum_five, sum;
+	int i;
+
+	sum_three = 0;
+	sum_five  = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; ++i)
+	{
+		if ((i % 3) == 0)
+		{
+			sum_three = sum_three + i;
+		} else if ((i % 5) == 0)
+		{
+			sum_five = sum_five + i;
+		}
+	}
+	sum = sum_three + sum_five;
+	printf("%lu\n", sum);
+	return (0);
+}
