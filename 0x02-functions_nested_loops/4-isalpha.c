e3f13018abcfa2e5c2a6625562343e2454b4ec15
M-Steve1 <modebestephen2@gmail.com>
2022-09-13 19:29:10 -0700

    Add 4-isalpha.c script

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for uppercase
+ *
+ * @c: The character to check for
+ *
+ * Return: return 1 if uppercase, 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
