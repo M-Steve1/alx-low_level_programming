1f8960124b34e4184b329eda5c0fc023aeccb3d0
M-Steve1 <modebestephen2@gmail.com>
2022-09-15 22:44:29 -0700

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - entry point
@@ -12,6 +13,7 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
+
 		if ((n % 3 == 0) && (n % 5 != 0))
 			printf("Fizz ");
 		else if ((n % 5 == 0) && (n % 3 != 0))
