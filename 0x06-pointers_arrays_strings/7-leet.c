5b2f795afe570fdfc67744d5e14e6e5afed34660
M-Steve1 <modebestephen2@gmail.com>
2022-09-22 17:36:46 -0700

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,16 @@
 
 char *leet(char *c)
 {
-	int i;
+	int i, j;
+
+	char *letters = "aAeEoOtTlL";
+	char *numbers = "4433007711";
 
 	for (i = 0; c[i] != '\0'; i++)
 	{
-		if (c[i] == 'a' || c[i] == 'A')
-			c[i] = '4';
-		else if (c[i] == 'e' || c[i] == 'E')
-			c[i] = '3';
-		else if (c[i] == 'o' || c[i] == 'O')
-			c[i] = '0';
-		else if (c[i] == 't' || c[i] == 'T')
-			c[i] = '7';
-		else if (c[i] == 'l' || c[i] == 'L')
-			c[i] = '1';
+		for (j = 0; letters[j] != '\0'; j++)
+			if (c[i] == letters[j])
+				c[i] = numbers[j];
 	}
 	return (c);
 }
