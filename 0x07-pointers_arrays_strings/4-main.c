180118ffe973f30a82ec7f78b6dd074eab067e45
M-Steve1 <modebestephen2@gmail.com>
2022-09-27 13:16:36 -0700

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-main.c
+++ 0x07-pointers_arrays_strings/4-main.c
@@ -8,15 +8,17 @@
  */
 int main(void)
 {
-/**	char *s = "hello, world";
+	char *s = "hello, world";
 	char *f = "world";
-*/	char *t;
+	char *t;
 
-/**
 	t = _strpbrk(s, f);
 	printf("%s\n", t);
-	*/
 	t = _strpbrk("First, solve the problem. Then, write the code.", "[]{}()");
+	printf("%s\n", t);
+	t = _strpbrk("abcdef", "");
+	printf("%s\n", t);
+       	t = _strpbrk("", "abcdef");
 	printf("%s\n", t);
 	return (0);
 }
