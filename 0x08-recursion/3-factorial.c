2851742567ddca94696831e9c30fb4719601cf83
M-Steve1 <modebestephen2@gmail.com>
2022-09-28 16:23:06 -0700

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _factorial - Calculate the factorial of a given number
+ * factorial - Calculate the factorial of a given number
  * @n: number to calculate factorial for
  *
  * Return: returns the factorial of a given number.
  */
-int _factorial(int n)
+int factorial(int n)
 {
 	if (n == 0 || n == 1)
 		return (1);
