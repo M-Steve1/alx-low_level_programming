c458806034f620c12be085f05f9be8e1ff97793b
M-Steve1 <modebestephen2@gmail.com>
2022-10-05 19:35:20 -0700

    Add c files

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,69 +1,49 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * str_concat - concatenates two strings
- * @s1: str to concat
- * @s2: str to concat
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
  *
- * Return: On Success returns a pointer to the newly allocated
- * space in memory and NULL on failure.
+ * Return: pointer to the new string created (Success), or NULL (Error)
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr;
-	unsigned int len1, len2, i, j;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	len1 = 0;
-	len2 = 0;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
+		return (NULL);
+
 	i = 0;
 	j = 0;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-
-	while (s1[len1] != '\0')
-		len1++;
-	while (s2[len2] != '\0')
-		len2++;
-
-	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (ptr == NULL)
-		return (NULL);
-	if (s1 == NULL)
+	if (s1)
 	{
-		while (s2[i] != '\0')
+		while (i < len1)
 		{
-			ptr[i] = s2[i];
+			s3[i] = s1[i];
 			i++;
 		}
-		ptr[i + 1] = '\0';
-		return (ptr);
 	}
-	else if (s2 == NULL)
+
+	if (s2)
 	{
-		while (s1[i] != '\0')
+		while (i < (len1 + len2))
 		{
-			ptr[i] = s1[i];
+			s3[i] = s2[j];
 			i++;
+			j++;
 		}
-		ptr[i + 1] = '\0';
-		return (ptr);
 	}
+	s3[i] = '\0';
 
-	while (s1[i] != '\0')
-	{
-		ptr[i] = s1[i];
-		i++;
-	}
-
-	while (s2[j] != '\0')
-	{
-		ptr[len1 + j] = s2[j];
-		j++;
-	}
-
-	ptr[len1 + len2 + 1] = '\0';
-
-	return (ptr);
+	return (s3);
 }
