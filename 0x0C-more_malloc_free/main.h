27c4e59b92e95f08063f89648a1e45b6a021f85c
M-Steve1 <modebestephen2@gmail.com>
2022-10-07 12:09:26 -0700

    Add _putchar function prototype

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,4 @@
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 void concat_s1_s2(unsigned int i, unsigned int j, char *s3, char *s1, char *s2);
 void n_byte_concat(unsigned int n_byte, unsigned int i, unsigned int j, char *s3, char *s1, char *s2);
