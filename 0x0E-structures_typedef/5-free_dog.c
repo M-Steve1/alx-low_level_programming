f829abe568ca0ed6334a9af2507107cc67cb782c
M-Steve1 <modebestephen2@gmail.com>
2022-10-10 17:49:48 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - frees dogs.
+ * @d: pointer to struct dog
+ *
+ * Return: returns nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	else
+		free(d);
+}
