285a482642041ca9c38df417c76ddbb90c276060
M-Steve1 <modebestephen2@gmail.com>
2022-10-12 14:45:22 -0700

    Add 4-sum.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,16 @@
+#ifndef FILENAME_H
+#define FILENAME_H
+#define FILENAME __FILE__
+
+#define ABS(x) ((x) * 1)
+
+#if i < 0
+	i = -i
+#endif
+#if i > 0
+	i = -i
+#endif
+
+
+#endif /* FILENAME_H */
+
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef FILENAME_H
+#define FILENAME_H
+#define FILENAME __FILE__
+#define SUM(x, y) ((x) + (y))
+
+#endif /* FILENAME_H */
