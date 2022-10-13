c9d8e67ce2cd887ffd66d24e2295a4b8be8c3d1c
M-Steve1 <modebestephen2@gmail.com>
2022-10-12 19:26:38 -0700

    Add 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches for an integer.
+ * @array: Pointer to array, where the integer will be
+ * searched for.
+ * @size: size of the array.
+ * @cmp: Pointer to the compare function.
+ *
+ * Return: Returns the integer index for the first element
+ * for which cmp function does not return 0, returns -1
+ * if no element matches and -1 if size is <= 0.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
+#include <stddef.h>
 #ifndef FILENAME_H
-#define FILENAME_HI
+#define FILENAME_H
 #define FILENAME __FILE__
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif /* FILENAME_H */
