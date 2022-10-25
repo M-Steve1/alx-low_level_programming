d9c47e46f5e3b55a7351d921b9f9f3a048ec240d
M-Steve1 <modebestephen2@gmail.com>
2022-10-25 13:18:40 -0700

    Add 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * before_main - This function excutes before the main
+ * funtion.
+ *
+ * Return: Returns Nothing.
+ */
+void before_main(void) __attribute__((constructor));
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
