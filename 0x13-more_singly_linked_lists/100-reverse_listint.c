ac66b89771baa71fd707188e2dfb08f365dc6879
M-Steve1 <modebestephen2@gmail.com>
2022-10-28 06:35:51 -0700

    Bug fix

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverses a listint_t linked list.
+ * @head: Double pointer to listint_t.
+ * 
+ * Return: Retruns a pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next = NULL;
+	listint_t *prev = NULL;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+
+
+	return (*head);
+}
---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - function that prints a linked list with a loop safely.
+ * @head: pointer to the 1st node of the linked list
+ * Return: new_node
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+const listint_t *temp, *ptr;
+size_t count, i;
+
+temp = NULL, ptr = NULL;
+
+count = 0;
+
+if (head == NULL)
+	exit(98);
+
+temp = head;
+
+while (temp)
+{
+printf("[%p] %d\n", (void *)temp, temp->n);
+count++;
+temp = temp->next;
+ptr = head;
+i = 0;
+while (i < count)
+{
+if (ptr == temp)
+{
+printf("[%p] %d\n", (void *)temp, temp->n);
+return (count);
+}
+i++;
+ptr = ptr->next;
+}
+}
+return (count);
+}
