fac18a8dc14ef4f3b5364e897d0029686694c196
M-Steve1 <modebestephen2@gmail.com>
2022-11-04 14:52:16 -0700

    Fix 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -76,7 +76,7 @@ int main(int ac, char **av)
 		if (file == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-			       "Error: Can't read from %s\n", av[1]);
+			       "Error: Can't read from file %s\n", av[1]);
 			free(buffer);
 			exit(98);
 		}
