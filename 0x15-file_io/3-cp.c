3fe3ac27e3a33d7317167b772caab9bc4f5af2b0
M-Steve1 <modebestephen2@gmail.com>
2022-11-04 08:44:52 -0700

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -52,3 +52,24 @@ int copy_file(char *file_from, char *file_to)
 		exit(100);
 	} return (1);
 }
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(int ac, char **av)
+{
+	int res;
+
+	if (ac != 3)
+	{
+		printf("Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	res = copy_file(av[1], av[2]);
+    	printf("-> %i)\n", res);
+
+	return (0);
+}
